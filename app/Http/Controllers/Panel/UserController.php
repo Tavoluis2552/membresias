927ec58209dd926fa4a7522adaeefe3eb1a3d55c
junior15 <junior15062023@gmail.com>
2025-05-03 00:08:59 -0500

    backend part 1

---
+++ app/Http/Controllers/Panel/UserController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Panel;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+    public function listUsers()
+    {
+        Gate::authorize('viewAny', User::class);
+        $users = User::with('local')->where('status', true)->paginate(1);
+        return response()->json([
+            'users' => UserResource::collection($users),
+        ]);
+    }
+
+
+    public function index() {}
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
